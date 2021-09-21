286fb41d94b3298ea58344dd9a76b4932ad2c31d
rakib2944 <rakib15-2944@diu.edu.bd>
2021-09-21 11:46:13 +0600

    Intro

--- src/App.js
+++ src/App.js
@@ -2,24 +2,49 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const allPersons = [
+
+    { name: 'faria', id: '193-15-2918', dept: 'CSE' },
+    { name: 'Rakib', id: '193-15-2944', dept: 'CSE' },
+    { name: 'apel', id: '193-15-2984', dept: 'CSE' },
+    { name: 'Nahid', id: '193-15-3006', dept: 'CSE' },
+    { name: 'Misu', id: '193-15-2974', dept: 'CSE' },
+    { name: 'Fahim', id: '193-15-2956', dept: 'CSE' },
+
+  ]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+      {
+        allPersons.map(person => <Person name={person.name} id={person.id} dept={person.dept}></Person>)
+      }
+
+      {/* <Person name='Apel Mahmud' id='193-15-2984' dept='CSE'></Person> */}
+
+
     </div>
   );
 }
+
+
+function Person(props) {
+  return (
+
+    <div className='Person'>
+
+      <h2>Name : {props.name}</h2>
+      <h3>ID :  {props.id}</h3>
+      <h4>Deperment : {props.dept}</h4>
+
+    </div>
+
+  );
+
+}
+
+
 
 export default App;
